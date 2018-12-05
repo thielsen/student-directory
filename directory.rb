c0604a5bea2cfc59d698ce3ee44723a3cca6b94a
Simon Green <me@greeny.me.uk>
2018-12-05 18:14:21 +0000

    Save data to csv

--- directory.rb
+++ directory.rb
@@ -34,7 +34,18 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+file.close
 end
 
 def show_students
@@ -49,6 +60,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -59,7 +72,6 @@ end
 def print_header
   puts "The students of Villians Academy"
   puts "------------------"
-  puts @students
 end
 
 def print_student_list
